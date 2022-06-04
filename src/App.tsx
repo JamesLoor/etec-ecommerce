747c1015b87eeeda0d61215e3e0e61cc8619f7af
James Mendoza <mendozajames02@hotmail.com>
2022-06-04 17:28:26 -0500

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,16 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import styled from 'styled-components'
 
-function App() {
-  const [count, setCount] = useState(0)
+const AppStyled = styled.div`
+  h1 {
+    font-size: 50px;
+  }
+`
 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <AppStyled>
+      <h1>Ecommerce Etec</h1>
+    </AppStyled>
   )
 }
 
