a13cef5638bfd6ceaa9c389ad542892265f8bcdd
James Mendoza <mendozajames02@hotmail.com>
2022-06-15 17:06:12 -0500

    Start to create productHeader component

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import styled from 'styled-components'
+import MagnifyingGlass from '../../icons/MagnifyingGlass'
 
 const ProductStyled = styled.div`
   /*  */
@@ -10,6 +11,16 @@ type Props = {}
 const Product: React.FC<Props> = () => {
   return (
     <ProductStyled>
+      <header className="product__header">
+        <div className="wrapper">
+          <div className="productHeader">
+            <div className="productSearch">
+              <MagnifyingGlass />
+              <input type="text" className="product__search" />
+            </div>
+          </div>
+        </div>
+      </header>
       <h1>Product</h1>
     </ProductStyled>
   )
