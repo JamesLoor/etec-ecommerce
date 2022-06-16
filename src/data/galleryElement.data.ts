46b2f952cf41ea4bd49967db2ebea54e5e8dd83b
unknown <derianpaez01@hotmail.com>
2022-06-15 22:38:30 -0500

    add gallery products section

---
+++ src/data/galleryElement.data.ts
@@ -0,0 +1,37 @@
+export const galleryElementData = [
+  {
+    id: '001',
+    className: 'products-1',
+    name: 'Electrodomesticos',
+    text: 'Cocinas, Refrigeradores, Extractores, Congeladores, etc.',
+    background: 'https://cdn.discordapp.com/attachments/402596575928385537/946985070093279312/home.png'
+  },
+  {
+    id: '002',
+    className: 'products-2',
+    name: 'Tecnología',
+    text: 'Celulares, Laptops, Tablets, etc.',
+    background: 'https://cdn.discordapp.com/attachments/402596575928385537/946985070579826748/tech.png'
+  },
+  {
+    id: '003',
+    className: 'products-3',
+    name: 'Vestimenta',
+    text: 'Gucci, HM, Nike, Adidas, Etc.',
+    background: 'https://cdn.discordapp.com/attachments/402596575928385537/946985071292854272/clothes.png'
+  },
+  {
+    id: '004',
+    className: 'products-4',
+    name: 'Maquillaje',
+    text: 'HM, Kilye Cosmetics, etc.',
+    background: 'https://cdn.discordapp.com/attachments/402596575928385537/946985071020236860/makeup.png'
+  },
+  {
+    id: '005',
+    className: 'products-5',
+    name: 'Salud',
+    text: 'Fybeca, Sana sana, Cruz Azul, etc.',
+    background: 'https://cdn.discordapp.com/attachments/402596575928385537/946985850468728832/cream.49b8815a8c5a6eb48d36.jpg'
+  }
+]
